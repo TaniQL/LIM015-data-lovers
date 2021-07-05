311096a1d509a86704a4aae38f9353e98e3f8ddf
MaffPerez <plzamfr@gmail.com>
2021-07-05 10:27:18 -0500

    Export PokemonName

--- src/data.js
+++ src/data.js
@@ -1,7 +1,11 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const example = (elemento) => {
+  return `<article class="cardpokemon">
+  <img src="" alt="" class="imgpokemon">
+  <h4 class="titulopokemon">${elemento.name}</h4>
+</article>`
+  ;
 };
 
 export const anotherExample = () => {
