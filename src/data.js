06cde50de4d9bba5543d1333578444fc0ee428b7
TaniQL <taniaquispelazo@gmail.com>
2021-07-06 09:25:20 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -21,21 +21,23 @@ if (elemento.evolution.hasOwnProperty("next-evolution")){
   }
 }
 
-  return `<article class="cardPokemon-front">
+  return `<article class="cardPokemon-front" id="cardPokemonFront">
   <h1 class="namePokemon">${elemento.name}</h1>
-  <h1 class="numPokemon">${elemento.num}</h1><br></br>
+  <h1 class="numPokemon">N°${elemento.num}</h1><br></br>
   <img src="${elemento.img}" alt="" class="imgPokemon">
   <h4 class="typePokemon">${elemento.type}</h4>
-</article>
+  </article>
 
 <article class="cardPokemon-back">
-<h2 class="region">${elemento.generation.name}</h2>
-<h2 class="resistant">${elemento.resistant}</h2>
-<h2 class="weakness">${elemento.weaknesses}</h2>
+<h2 class="region">Region:${elemento.generation.name}</h2>
+<h2 class="resistant">Resistant:${elemento.resistant}</h2>
+<h2 class="weakness">Weakness:${elemento.weaknesses}</h2><br></br>
+<h2>Evolutions</h2>
 <h3 class="evolution">${evolution}</h3>
 <h3 class="next-evolution">${nextEvolution}</h3>
 <h3 class="prev-evolution">${prevEvolution}</h3>
 <h3 class="prev-evolution">${prevPrevEvolution}</h3><br></br>
+<input class="moreInfo" type="button" id="btnMoreInfo" value="More Info">
 </article>
 `
   ;
--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import data from './data/pokemon/pokemon.js';
 
 /** Mostrar y ocultar secciones del HTML**/
 const btnPokedex = document.getElementById("btnPokedex");
+//const btnMoreInfo =document.querySelectorAll('#moreInfo');
 
 btnPokedex.addEventListener("click",() => {
 let home = document.getElementById("home");
 home.style.display="none";
-
 });
 
-
 document.getElementById("pokedex").innerHTML = data.pokemon.map(dataPokemon).join(" ");
+
+// btnMoreInfo.addEventListener("click",() => {
+// let cardFront = document.querySelector('#cardPokemonFront');
+// cardFront.style.display="none";
+// });
