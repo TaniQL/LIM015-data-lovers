0a14ff51d59d2d4492aa823b130d6bbe2a7292f5
MaffPerez <plzamfr@gmail.com>
2021-07-06 19:38:15 -0500

    Css home

--- src/main.js
+++ src/main.js
@@ -4,13 +4,25 @@ import data from './data/pokemon/pokemon.js';
 
 /** Mostrar y ocultar secciones del HTML**/
 const btnPokedex = document.getElementById("btnPokedex");
-
+const btnHome = document.getElementById("btnHome");
+const home = document.getElementById("home");
+const pokedex = document.getElementById("pokedex");
+const footer = document.getElementById("container-footer");
 
 btnPokedex.addEventListener("click",() => {
-let home = document.getElementById("home");
 home.style.display="none";
+pokedex.style.display="block";
+footer.style.display="block"
+
 });
 
+btnHome.addEventListener("click",() => {
+pokedex.style.display="none";
+home.style.display="block";
+footer.style.display="none";
+});
+
+//Extra la data del pokemon js
 document.getElementById("pokedex").innerHTML = data.pokemon.map(dataPokemon).join(" ");
 // const btnMoreInfo =document.querySelector('#btnMoreInfo');
 // btnMoreInfo.addEventListener("click",() => {
