1e370fd43c98e7008fc3119d6b67eaab92d35f83
TaniQL <taniaquispelazo@gmail.com>
2021-07-06 23:32:23 -0500

    Update Pokemon Container

--- src/data.js
+++ src/data.js
@@ -22,14 +22,14 @@ export const dataPokemon = (elemento) => {
     }
   }
 
-  return `<article class="cardPokemon-front" id="cardPokemonFront">
+  return `<div class="cardPokemon-front" id="cardPokemonFront">
   <h1 class="namePokemon">${elemento.name}</h1>
   <h1 class="numPokemon">N°${elemento.num}</h1><br></br>
   <img src="${elemento.img}" alt="" class="imgPokemon">
   <h4 class="typePokemon">${elemento.type}</h4>
-  </article>
+  </div>
 
- <article class="cardPokemon-back">
+ <div class="cardPokemon-back">
   <h2 class="region">Region:${elemento.generation.name}</h2>
   <h2 class="resistant">Resistant:${elemento.resistant}</h2>
   <h2 class="weakness">Weakness:${elemento.weaknesses}</h2><br></br>
@@ -39,7 +39,7 @@ export const dataPokemon = (elemento) => {
   <h3 class="prev-evolution">${prevEvolution}</h3>
   <h3 class="prev-evolution">${prevPrevEvolution}</h3><br></br>
   <input class="moreInfo" type="button" id="btnMoreInfo" value="More Info">
-  </article>
+  </div>
   `
   ;
 };
--- src/main.js
+++ src/main.js
@@ -8,18 +8,21 @@ const btnHome = document.getElementById("btnHome");
 const home = document.getElementById("home");
 const pokedex = document.getElementById("pokedex");
 const footer = document.getElementById("container-footer");
+//const imgPikachu = document.getElementById("imgPikachu");
+
 
 btnPokedex.addEventListener("click",() => {
 home.style.display="none";
 pokedex.style.display="block";
-footer.style.display="block"
-
+footer.style.display="block";
+//imgPikachu.style.visibility = "hidden";
 });
 
 btnHome.addEventListener("click",() => {
 pokedex.style.display="none";
 home.style.display="block";
 footer.style.display="none";
+//imgPikachu.style.visibility = "active";
 });
 
 //Extra la data del pokemon js
