a740fadec97321d272f067d58eb57145753bba67
MaffPerez <plzamfr@gmail.com>
2021-07-07 13:09:34 -0500

    Update css pokedex

--- src/main.js
+++ src/main.js
@@ -6,27 +6,28 @@ import data from './data/pokemon/pokemon.js';
 const btnPokedex = document.getElementById("btnPokedex");
 const btnHome = document.getElementById("btnHome");
 const home = document.getElementById("home");
-const pokedex = document.getElementById("pokedex");
+const containerPokedex = document.getElementById("containerPokedex");
 const footer = document.getElementById("container-footer");
+
 //const imgPikachu = document.getElementById("imgPikachu");
 
 
 btnPokedex.addEventListener("click",() => {
 home.style.display="none";
-pokedex.style.display="block";
+containerPokedex.style.display="block";
 footer.style.display="block";
 //imgPikachu.style.visibility = "hidden";
 });
 
 btnHome.addEventListener("click",() => {
-pokedex.style.display="none";
+containerPokedex.style.display="none";
 home.style.display="block";
 footer.style.display="none";
 //imgPikachu.style.visibility = "active";
 });
 
 //Extra la data del pokemon js
-document.getElementById("pokedex").innerHTML = data.pokemon.map(dataPokemon).join(" ");
+document.getElementById("cardPokedex").innerHTML = data.pokemon.map(dataPokemon).join(" ");
 // const btnMoreInfo =document.querySelector('#btnMoreInfo');
 // btnMoreInfo.addEventListener("click",() => {
 // let cardFront = document.querySelector('#cardPokemonFront');
