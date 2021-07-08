fc1917c9ff748f93904eb5d400381a14fb921efe
MaffPerez <plzamfr@gmail.com>
2021-07-07 23:00:02 -0500

    Update css & html

--- src/data.js
+++ src/data.js
@@ -2,48 +2,48 @@
 // estas funciones son de ejemplo
 
 export const dataPokemon = (elemento) => {
-  let evolution = '';
-  let nextEvolution = '';
-  let prevEvolution = '';
-  let prevPrevEvolution = '';
-
-  if (elemento.evolution.hasOwnProperty("next-evolution")){
-    evolution = elemento.evolution["next-evolution"][0].name;
-
-    if(elemento.evolution["next-evolution"][0].hasOwnProperty("next-evolution")){
-    nextEvolution = elemento.evolution["next-evolution"][0]["next-evolution"][0].name;
-    } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
-      prevEvolution = elemento.evolution["prev-evolution"][0].name;
-    }
-  } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
-    prevEvolution = elemento.evolution["prev-evolution"][0].name;
-    if(elemento.evolution["prev-evolution"][0].hasOwnProperty("prev-evolution")){
-    prevPrevEvolution = elemento.evolution["prev-evolution"][0]["prev-evolution"][0].name;
-    }
-  }
 
   return `<div class="cardPokemon-front" id="cardPokemonFront">
   <h1 class="namePokemon">${elemento.name}</h1>
   <h1 class="numPokemon">N°${elemento.num}</h1><br></br>
   <img src="${elemento.img}" alt="" class="imgPokemon">
   <h4 class="typePokemon">${elemento.type}</h4>
-  </div>
-
-  <div class="cardPokemon-back">
-  <h2 class="region">Region:${elemento.generation.name}</h2></br>
-  <h2 class="resistant">Resistant:${elemento.resistant}</h2></br>
-  <h2 class="weakness">Weakness:${elemento.weaknesses}</h2><br></br>
-  <h2>Evolutions</h2></br>
-  <h3 class="evolution">${evolution}</h3></br>
-  <h3 class="next-evolution">${nextEvolution}</h3></br>
-  <h3 class="prev-evolution">${prevEvolution}</h3></br>
-  <h3 class="prev-evolution">${prevPrevEvolution}</h3><br>
-  <input class="moreInfo" type="button" id="btnMoreInfo" value="More Info">
-  </div>
-  `
+  </div>`
 };
 
 
   export const anotherExample = () => {
     return 'OMG';
   };
+
+
+  // let evolution = '';
+  // let nextEvolution = '';
+  // let prevEvolution = '';
+  // let prevPrevEvolution = '';
+
+  // if (elemento.evolution.hasOwnProperty("next-evolution")){
+  //   evolution = elemento.evolution["next-evolution"][0].name;
+
+  //   if(elemento.evolution["next-evolution"][0].hasOwnProperty("next-evolution")){
+  //   nextEvolution = elemento.evolution["next-evolution"][0]["next-evolution"][0].name;
+  //   } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
+  //     prevEvolution = elemento.evolution["prev-evolution"][0].name;
+  //   }
+  // } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
+  //   prevEvolution = elemento.evolution["prev-evolution"][0].name;
+  //   if(elemento.evolution["prev-evolution"][0].hasOwnProperty("prev-evolution")){
+  //   prevPrevEvolution = elemento.evolution["prev-evolution"][0]["prev-evolution"][0].name;
+  //   }
+  // }
+
+  // <div class="cardPokemon-back">
+  // <h2 class="region">Region:${elemento.generation.name}</h2></br>
+  // <h2 class="resistant">Resistant:${elemento.resistant}</h2></br>
+  // <h2 class="weakness">Weakness:${elemento.weaknesses}</h2><br></br>
+  // <h2>Evolutions</h2></br>
+  // <h3 class="evolution">${evolution}</h3></br>
+  // <h3 class="next-evolution">${nextEvolution}</h3></br>
+  // <h3 class="prev-evolution">${prevEvolution}</h3></br>
+  // <h3 class="prev-evolution">${prevPrevEvolution}</h3><br>
+  // <input class="moreInfo" type="button" id="btnMoreInfo" value="More Info">
