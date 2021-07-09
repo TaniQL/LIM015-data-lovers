72f2159d6c9bba6e1fd3cbf2151daf729dacc00a
TaniQL <taniaquispelazo@gmail.com>
2021-07-09 03:03:04 -0500

    Change CardPokemonBack

--- src/data.js
+++ src/data.js
@@ -1,49 +1,66 @@
 /* eslint-disable no-prototype-builtins */
 // estas funciones son de ejemplo
 
-export const dataPokemon = (elemento) => {
+/*Cara Delantera de la tarjeta*/
+
+export const dataPokemonFront = (elemento) => {
 
   return `<div class="cardPokemon-front" id="cardPokemonFront">
   <h1 class="namePokemon">${elemento.name}</h1>
   <h1 class="numPokemon">N°${elemento.num}</h1><br></br>
   <img src="${elemento.img}" alt="" class="imgPokemon">
-  <h4 class="typePokemon">${elemento.type}</h4>
+  <h1 class="typePokemon">${elemento.type}</h1>
   </div>`
 };
 
-
-  export const anotherExample = () => {
-    return 'OMG';
-  };
+/*Cara Delantera de la tarjeta*/
 
 
-  // let evolution = '';
-  // let nextEvolution = '';
-  // let prevEvolution = '';
-  // let prevPrevEvolution = '';
+export const dataPokemonBack = (elemento) => {
 
-  // if (elemento.evolution.hasOwnProperty("next-evolution")){
-  //   evolution = elemento.evolution["next-evolution"][0].name;
+  let evolution = '';
+  let nextEvolution = '';
+  let prevEvolution = '';
+  let prevPrevEvolution = '';
 
-  //   if(elemento.evolution["next-evolution"][0].hasOwnProperty("next-evolution")){
-  //   nextEvolution = elemento.evolution["next-evolution"][0]["next-evolution"][0].name;
-  //   } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
-  //     prevEvolution = elemento.evolution["prev-evolution"][0].name;
-  //   }
-  // } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
-  //   prevEvolution = elemento.evolution["prev-evolution"][0].name;
-  //   if(elemento.evolution["prev-evolution"][0].hasOwnProperty("prev-evolution")){
-  //   prevPrevEvolution = elemento.evolution["prev-evolution"][0]["prev-evolution"][0].name;
-  //   }
-  // }
+  if (elemento.evolution.hasOwnProperty("next-evolution")){
+    evolution = elemento.evolution["next-evolution"][0].name;
 
-  // <div class="cardPokemon-back">
-  // <h2 class="region">Region:${elemento.generation.name}</h2></br>
-  // <h2 class="resistant">Resistant:${elemento.resistant}</h2></br>
-  // <h2 class="weakness">Weakness:${elemento.weaknesses}</h2><br></br>
-  // <h2>Evolutions</h2></br>
-  // <h3 class="evolution">${evolution}</h3></br>
-  // <h3 class="next-evolution">${nextEvolution}</h3></br>
-  // <h3 class="prev-evolution">${prevEvolution}</h3></br>
-  // <h3 class="prev-evolution">${prevPrevEvolution}</h3><br>
-  // <input class="moreInfo" type="button" id="btnMoreInfo" value="More Info">
+    if(elemento.evolution["next-evolution"][0].hasOwnProperty("next-evolution")){
+    nextEvolution = elemento.evolution["next-evolution"][0]["next-evolution"][0].name;
+    } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
+      prevEvolution = elemento.evolution["prev-evolution"][0].name;
+    }
+  } else if (elemento.evolution.hasOwnProperty("prev-evolution")){
+    prevEvolution = elemento.evolution["prev-evolution"][0].name;
+    if(elemento.evolution["prev-evolution"][0].hasOwnProperty("prev-evolution")){
+    prevPrevEvolution = elemento.evolution["prev-evolution"][0]["prev-evolution"][0].name;
+    }
+  }
+
+  return `<div class="cardPokemon-back" id="cardPokemonBack">
+  <h1 class="region">Region:${elemento.generation.name}</h1>
+  <h1 class="resistant">Resistant:${elemento.resistant}</h1>
+  <h1 class="weakness">Weakness:${elemento.weaknesses}</h1><br></br>
+  <h2>Evolutions</h2>
+  <h1 class="evolution">${evolution}</h1>
+  <h1 class="next-evolution">${nextEvolution}</h1>
+  <h1 class="prev-evolution">${prevEvolution}</h1>
+  <h1 class="prev-evolution">${prevPrevEvolution}</h1>
+  </div>`
+};
+
+//<input class="moreInfo" type="button" id="btnMoreInfo" value="More Info">
+
+
+// // Función filter.../
+//   export const filterByRegion = (elemento) => {
+//     let dataFilter = elemento.filter(elemento => elemento.generation.name ==='kanto');
+//     return dataFilter
+//   };
+
+    // if(elemento.generation.name ==='kanto'){
+    // return console.log(elemento.filter(predicado));
+    // } else if(elemento.generation.name ==='johto'){
+    // return console.log("Región Johto");
+    // }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { dataPokemon } from './data.js';
+import { dataPokemonFront, dataPokemonBack } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
@@ -8,7 +8,11 @@ const btnHome = document.getElementById("btnHome");
 const home = document.getElementById("home");
 const containerMainPokedex = document.getElementById("containerMainPokedex");
 const footer = document.getElementById("container-footer");
+const cardPokemon = document.getElementById("cardsPokemon");
+const cardFrontPokemon = document.getElementById("cardFrontPokemon");
+const cardBackPokemon = document.getElementById("cardBackPokemon");
 
+//const selectRegion = document.getElementById("listTypeRegion");
 //const imgPikachu = document.getElementById("imgPikachu");
 
 
@@ -26,10 +30,44 @@ footer.style.display="none";
 //imgPikachu.style.visibility = "active";
 });
 
-//Extra la data del pokemon js
-document.getElementById("cardFrontPokemon").innerHTML = data.pokemon.map(dataPokemon).join(" ");
+//Extra la data del pokemon js a una  la parte delantera de la tarjeta Pokemon
+document.getElementById("cardFrontPokemon").innerHTML = data.pokemon.map(dataPokemonFront).join(" ");
+
+//Extraer la data del pokemon JS a la parte trasera de la tarjeta Pokemon
+document.getElementById("cardBackPokemon").innerHTML = data.pokemon.map(dataPokemonBack).join(" ");
+
+
+
 // const btnMoreInfo =document.querySelector('#btnMoreInfo');
 // btnMoreInfo.addEventListener("click",() => {
 // let cardFront = document.querySelector('#cardPokemonFront');
 // cardFront.style.display="none";
 // });
+
+// // Filtrar Data por Region
+// selectRegion.addEventListener("change", () => {
+//     let region = selectDirector.value;
+//     if (region == 'kanto') {
+//         return console.log("seleccionaste Kanto");
+//     } else {
+//         return console.log("seleccionaste Johto");
+//     }
+// });
+
+const mostrarFrente = () => {
+	if(cardsPokemon.classList.contains('active')){
+		cardsPokemon.classList.remove('active');
+	}
+}
+// const mostrarBack = () => {
+//     if(cardPokemon-back.classList.contains('active')){
+// 		cardPokemon-back.classList.remove('active');
+// 	}
+// }
+
+// * Rotacion del cardPokemon
+cardsPokemon.addEventListener('click', () => {
+    cardsPokemon.classList.toggle('active');
+    return console.log("puedegirar");
+});
+
